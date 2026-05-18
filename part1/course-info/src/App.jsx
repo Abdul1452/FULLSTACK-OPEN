7596be1a98a25f5e38ed8c0696502f12383dad3a
ABDULLAHI OLAMILEKAN ABDULWASIU <abdullahi.abdulwasiu@edu.turkuamk.fi>
2026-05-18 12:31:42 +0300

    Part 1: exercise 1.2 - refactor to array of objects

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -5,13 +5,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.excercises1}
+        {props.content[0].name} {props.content[0].exercises}
       </p>
       <p>
-        {props.part2} {props.excercises2}
+        {props.content[1].name} {props.content[1].exercises}
       </p>
       <p>
-        {props.part3} {props.excercises3}
+        {props.content[2].name} {props.content[2].exercises}
       </p>
     </div>
   );
@@ -21,27 +21,24 @@ const Total = (props) => {
 };
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const content = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        excercises1={exercises1}
-        part2={part2}
-        excercises2={exercises2}
-        part3={part3}
-        excercises3={exercises3}
+      <Content content={content} />
+      <Total
+        total={
+          content[0].exercises + content[1].exercises + content[2].exercises
+        }
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
-};
+}
+
 
 export default App;
