e90a2285a918e76a99dcd55c47f43ea66618a8e4
ABDULLAHI OLAMILEKAN ABDULWASIU <abdullahi.abdulwasiu@edu.turkuamk.fi>
2026-06-01 11:54:44 +0300

    Part 1: exercise 1.5 - wrap UI in Course component

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,5 +1,4 @@
 const Header = ({ course }) => {
-  console.log(course);
   return <h1>{course}</h1>;
 };
 const Content = ({ parts }) => {
@@ -16,32 +15,37 @@ const Content = ({ parts }) => {
 const Total = ({ total }) => {
   return <p>Number of exercises {total}</p>;
 };
-const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-    { name: "Redux", exercises: 11 },
-  ];
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-
+const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={totalExercises} />
     </div>
   );
 };
 
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+  return <Course course={course} />;
+}
+
+  
 export default App;
