02bd75bb834a0961b3b37d648d175105d9f46a6e
ABDULLAHI OLAMILEKAN ABDULWASIU <abdullahi.abdulwasiu@edu.turkuamk.fi>
2026-08-25 15:35:34 +0300

    Part 2: exercises 2.1-2.3 - courseinfo with proper keys and reduce total

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  );
+};
+const Total = ({ total }) => {
+  return <p>Number of exercises {total}</p>;
+};
+const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+  return <Course course={course} />;
+}
+
+  
+export default App;
